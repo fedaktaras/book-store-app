78ff202c36cca5b16ca4356d5259620ef622b9dd
Taras Fedak <tarasfedak@me.com>
2024-04-23 10:04:50 +0300

    Initial configurations of database. Added book entity. (#1)
    * Initial configurations of database. Added book entity.
    
    application.properties configuration added.
    Book Entity added.
    BookService and BookRepository added.

---
+++ src/main/java/com/example/bookstoreapp/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,49 @@
+package com.example.bookstoreapp.repository.impl;
+
+import com.example.bookstoreapp.model.Book;
+import com.example.bookstoreapp.repository.BookRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final EntityManagerFactory entityManagerFactory;
+
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            if (book.getId() == null) {
+                entityManager.persist(book);
+            } else {
+                entityManager.merge(book);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new RuntimeException("Can`t create or merge Book", e);
+        } finally {
+            entityManager.close();
+        }
+        return book;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            return entityManager.createQuery("FROM Book", Book.class)
+                    .getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Can`t get all Books", e);
+        }
+    }
+}
---
+++ src/main/java/com/example/bookstoreapp/servive/impl/BookServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.bookstoreapp.servive.impl;
+
+import com.example.bookstoreapp.model.Book;
+import com.example.bookstoreapp.repository.BookRepository;
+import com.example.bookstoreapp.servive.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
