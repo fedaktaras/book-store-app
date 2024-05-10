9592efd57ef1dc211e752a0da8d57a2657b8b64e
Taras Fedak <tarasfedak@me.com>
2024-05-10 17:59:54 +0300

    Security implemented

--- src/main/java/com/example/bookstoreapp/model/User.java
+++ src/main/java/com/example/bookstoreapp/model/User.java
@@ -2,15 +2,23 @@ package com.example.bookstoreapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.util.Collection;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Getter
@@ -18,7 +26,7 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id=?")
 @SQLRestriction("is_deleted = false")
 @Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -41,4 +49,41 @@ public class User {
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id")
+    )
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !isDeleted;
+    }
 }
--- src/main/java/com/example/bookstoreapp/servive/impl/UserServiceImpl.java
+++ src/main/java/com/example/bookstoreapp/servive/impl/UserServiceImpl.java
@@ -4,10 +4,16 @@ import com.example.bookstoreapp.dto.UserRegistrationRequestDto;
 import com.example.bookstoreapp.dto.UserResponseDto;
 import com.example.bookstoreapp.exception.RegistrationException;
 import com.example.bookstoreapp.mapper.UserMapper;
+import com.example.bookstoreapp.model.Role;
 import com.example.bookstoreapp.model.User;
+import com.example.bookstoreapp.repository.RoleRepository;
 import com.example.bookstoreapp.repository.UserRepository;
 import com.example.bookstoreapp.servive.UserService;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +21,8 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto userRegistrationRequestDto)
@@ -23,6 +31,11 @@ public class UserServiceImpl implements UserService {
             throw new RegistrationException("User with such email already exists");
         }
         User newUser = userMapper.registerUserDtoToUser(userRegistrationRequestDto);
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+        Role userRole = roleRepository.findByRole(Role.RoleName.USER)
+                .orElseThrow(() -> new RegistrationException("Default role not found"));
+        newUser.setRoles(new HashSet<>(Collections.singletonList(userRole)));
+        newUser.setRoles(Set.of(userRole));
         User savedUser = userRepository.save(newUser);
         return userMapper.userToUserResponseDto(savedUser);
     }
