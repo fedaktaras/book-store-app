e1a8f5b1a77a452a54b538cf8b77fd34c2b504d9
Taras Fedak <tarasfedak@me.com>
2024-05-17 10:00:37 +0300

    Add category implemented
    N + 1 not resolved yet

--- src/main/java/com/example/bookstoreapp/dto/BookRequestDto.java
+++ src/main/java/com/example/bookstoreapp/dto/BookRequestDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.Set;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
 
@@ -25,4 +26,5 @@ public class BookRequestDto {
     private String description;
     @NotNull
     private String coverImage;
+    private Set<Long> categoryIds;
 }
