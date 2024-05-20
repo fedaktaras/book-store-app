5bf746950dbc491a60b676f1641a526ca2b9a127
Taras Fedak <tarasfedak@me.com>
2024-05-20 07:15:53 +0300

    Add category implemented
    Mapping Category into Book
    N + 1 resolved

--- src/main/java/com/example/bookstoreapp/repository/BookRepository.java
+++ src/main/java/com/example/bookstoreapp/repository/BookRepository.java
@@ -2,7 +2,10 @@ package com.example.bookstoreapp.repository;
 
 import com.example.bookstoreapp.model.Book;
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +16,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("SELECT b FROM Book b JOIN b.categories c "
             + "WHERE c.id = :categoryId AND b.isDeleted = false")
     List<Book> findAllByCategoryId(@Param("categoryId") Long categoryId, Pageable pageable);
+
+    @EntityGraph(value = "book-with-categories", type = EntityGraph.EntityGraphType.LOAD)
+    Optional<Book> findById(Long id);
+
+    @EntityGraph(value = "book-with-categories", type = EntityGraph.EntityGraphType.LOAD)
+    Page<Book> findAll(Pageable pageable);
 }
